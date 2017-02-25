b1787494eb6d3ba293f7fad53889f4b9accd6836
Seaker <seaker000@gmail.com>
2017-02-25 02:07:26 +0200

    Device list with markers and click animation, last device position from DB

--- MomIsWatching/Controllers/IndexController.cs
+++ MomIsWatching/Controllers/IndexController.cs
@@ -1,18 +1,52 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using MomIsWatching.Models;
 
 namespace MomIsWatching.Controllers
 {
     public class IndexController : Controller
     {
-        private DeviceContext DbContext { get; set; }
+        private DeviceContext DbContext { get; } = new DeviceContext();
 
         // GET: Home
         public ActionResult Index()
         {
-            DbContext = new DeviceContext();
-
             return View(DbContext);
+        }
+
+        public ActionResult DeviceLastPosition()
+        {
+            List<DeviceLog> devices = new List<DeviceLog>(); 
+
+            if(DbContext.DeviceLogs.ToList().Any())
+            {
+                foreach (Device device in DbContext.Devices.ToList())
+                {
+                   devices.Add(DbContext.DeviceLogs.ToList().Last(x => x.DeviceId == device.Id));
+                }
+            }
+            
+            return PartialView(devices);
+        }
+
+        public JsonResult GetDeviceLastPosition()
+        {
+
+            List<DeviceLog> devices = new List<DeviceLog>();
+
+            if (DbContext.DeviceLogs.ToList().Any())
+            {
+                foreach (Device device in DbContext.Devices.ToList())
+                {
+                    List<DeviceLog> temp = DbContext.DeviceLogs.ToList().Where(x => x.DeviceId == device.Id).ToList();
+
+                    if (temp.Any())
+                        devices.Add(temp.Last());
+                }
+            }
+
+            return Json(devices, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
